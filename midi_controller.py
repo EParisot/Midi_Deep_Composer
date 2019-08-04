f9190941a4cc37262f78fc8f7fa011e3e69b4cce
eparisot <eparisot@student.42.fr>
2019-08-04 17:40:38 +0200

    offsets, velocities Signed-off-by: eparisot <eparisot@student.42.fr>

--- midi_controller.py
+++ midi_controller.py
@@ -3,13 +3,18 @@
 import logging
 import sys
 import time
+from collections import deque
 
 from rtmidi import MidiOut
 from rtmidi.midiutil import open_midiinput
+from rtmidi.midiconstants import NOTE_ON, NOTE_OFF
+
+seq_len = 32
+bpm = 120/60
+time_sign = [4, 4]
 
 midiout = MidiOut()
 available_ports = midiout.get_ports()
-
 if available_ports:
     midiout.open_port(0)
 else:
@@ -22,15 +27,31 @@ logging.basicConfig(level=logging.DEBUG)
 class MidiInputHandler(object):
     def __init__(self, port):
         self.port = port
-        self._wallclock = time.time()
+        self.dq = deque(maxlen=seq_len)
+        self._wallclock = 0
+        self.curr_events = {}
 
     def __call__(self, event, data=None):
         message, deltatime = event
-        self._wallclock += deltatime
-        print("[%s] @%0.6f %r" % (self.port, self._wallclock, message), flush=True)
+        # playback
         midiout.send_message(message)
-        #time.sleep(deltatime)
-
+        # process
+        self.process_msg(message, deltatime)
+        
+    def process_msg(self, message, deltatime):
+        self._wallclock += deltatime
+        if message[0] & 0xF0 == NOTE_ON:
+            status, note, velocity = message
+            parsed_msg = [note, 0, deltatime * bpm, velocity]
+            self.curr_events[note] = parsed_msg
+        if message[0] & 0xF0 == NOTE_OFF:
+            status, note, velocity = message
+            self.curr_events[note][1] = deltatime * bpm
+            self.dq.append(self.curr_events[note])
+            self.curr_events.pop(note)
+        
+        print("[%s] @%0.6f %r" % (self.port, self._wallclock, message), flush=True)
+        print(self.dq, flush=True)
 
 # Prompts user for MIDI input port, unless a valid port number or name
 # is given as the first argument on the command line.
